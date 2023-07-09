038de4703c54f88830debb86794ddc4ee0291c96
Guilherme Salustiano <guissalustiano@gmail.com>
2023-07-09 17:50:30 -0300

    :sparkles: smart shell

--- src/bin/main.cpp
+++ src/bin/main.cpp
@@ -1,33 +1,40 @@
+#include <regex>
 #include <string>
 
 #include "process_manager.hpp"
+
+
+const std::regex CREATE_COMMAND("c(reate) -i ([0-9]+) -m ([0-9]+)"); // TODO: handle if "-m" befor "-i"
+const std::regex RUN_COMMAND("(r(un)?)?");
+const std::regex KILL_COMMAND("k(ill)? ([0-9]+)");
+const std::regex HELP_COMMAND("h(elp)?");
+const std::regex EXIT_COMMAND("e(xit)?");
 
 int main() {
     Memory memory;
     ProcessManager process_manager(memory);
 
     std::string input_command;
+    std::smatch input_matchs;
 
     while (true) {
         std::cout << memory << std::endl;
         std::cout << process_manager << std::endl;
-        std::cin >> input_command;
 
-        if (input_command == "create") {
-            uint16_t instruction_amount, memory_size;
+        std::getline (std::cin,input_command);
 
-            std::cin >> instruction_amount >> memory_size;
+        if (std::regex_match(input_command, input_matchs, CREATE_COMMAND)) {
+            uint16_t instruction_amount = std::stoi(input_matchs[2]);
+            uint16_t memory_size = std::stoi(input_matchs[3]);
 
             process_manager.plan_create(instruction_amount, memory_size);
-        } else if (input_command == "kill") {
-            uint16_t process_id;
-
-            std::cin >> process_id;
+        } else if (std::regex_match(input_command, input_matchs, KILL_COMMAND)){
+            uint16_t process_id = std::stoi(input_matchs[2]);
 
             process_manager.plan_kill(process_id);
-        } else if (input_command == "exit") {
+        } else if (std::regex_match(input_command, input_matchs, EXIT_COMMAND)) {
             break;
-        } else {
+        } else if (std::regex_match(input_command, input_matchs, RUN_COMMAND)) {
             try {
                 process_manager.run_process();
             } catch (std::out_of_range& e) {
@@ -35,6 +42,13 @@ int main() {
             } catch (std::runtime_error& e) {
                 std::cout << "Not enough memory to create process" << std::endl;
             }
+        } else if (std::regex_match(input_command, input_matchs, HELP_COMMAND)){
+          std::cout << "<empty>|r|run: clock" << std::endl;
+          std::cout << "create -i <instruction_amount> -m <memory_size>: create process" << std::endl;
+          std::cout << "kill <process_id>: kill process" << std::endl;
+        } else {
+          std::cout << "Invalid command " << input_command << std::endl;
+          std::cout << "use \"help\" to get options" << std::endl;
         }
 
         std::cout << "------------------------" << std::endl;
